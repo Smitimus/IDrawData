4d7c18f4effdb75f20cc59f1e90611166cb9115d
Smitimus <smity@bridgebum.net>
2023-10-23 01:04:14 -0400

    Update product.sql

--- sql/public/product.sql
+++ sql/public/product.sql
@@ -1,94 +1,87 @@
 /* Drops the table, but only if it exists */
-DROP TABLE IF EXISTS PUBLIC.PRODUCT;
+DROP TABLE IF EXISTS PUBLIC.PRODUCTS;
 
 /* Creates the table using the columns specified below */
 CREATE TABLE
-    PUBLIC.PRODUCT (
+    PUBLIC.PRODUCTS (
         PRODUCT_UPC integer,
         PRODUCT_NAME VARCHAR(200),
         PRODUCT_MANUFACTURER VARCHAR(200),
         PRODUCT_CLASS VARCHAR(200),
         PRODUCT_AVERAGE_COST DECIMAL(20, 2),
         PRODUCT_REGULAR_PRICE DECIMAL(20, 2),
-        PRODUCT_CURRENT_PRICE DECIMAL(20, 2),
-        PRODUCT_STARTING_QTY integer
+        PRODUCT_CURRENT_PRICE DECIMAL(20, 2)
     );
 
 /* Inserts data into the table */
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001001',
-    'Burp 1lt Btl',
-    'Burp Soda',
+    'BURP 1lt Btl',
+    'BURP Soda',
     'Beverage',
     '1.25',
     '1.99',
-    '1.79',
-    '37';
+    '1.79';
 
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001002',
-    'Burp 2lt Btl',
-    'Burp Soda',
+    'BURP 2lt Btl',
+    'BURP Soda',
     'Beverage',
     '1.88',
     '2.99',
-    '2.39',
-    '14';
+    '2.39';
 
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001003',
-    'Burp 20oz Btl',
-    'Burp Soda',
+    'BURP 20oz Btl',
+    'BURP Soda',
     'Beverage',
     '0.99',
     '1.99',
-    '1.39',
-    '83';
+    '1.39';
 
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001004',
-    'Burp 6pk 16oz Btl',
-    'Burp Soda',
+    'BURP 6pk 16oz Btl',
+    'BURP Soda',
     'Beverage',
     '1.99',
     '3.99',
-    '2.99',
-    '38';
+    '2.99';
 
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001005',
-    'Burp 12pk 12oz Cans',
-    'Burp Soda',
+    'BURP 12pk 12oz Cans',
+    'BURP Soda',
     'Beverage',
     '3.99',
     '6.99',
-    '4.79',
-    '18';
+    '4.79';
 
 INSERT INTO
-    PUBLIC.PRODUCT
+    PUBLIC.PRODUCTS
 SELECT
     '10001006',
-    'Burp 6pk 12oz Cans',
-    'Burp Soda',
+    'BURP 6pk 12oz Cans',
+    'BURP Soda',
     'Beverage',
     '2.19',
     '3.99',
-    '2.69',
-    '24';
+    '2.69';
 
 /* Select * shows us all the columns and rows in the table */
 SELECT
     *
 FROM
-    PUBLIC.PRODUCT;
+    PUBLIC.PRODUCTS;
